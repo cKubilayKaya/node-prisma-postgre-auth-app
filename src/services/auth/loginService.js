889a36c6dc8df67093f093f5fa859013a788c832
cKubilayKaya <101182731+cKubilayKaya@users.noreply.github.com>
2025-03-22 13:47:51 +0300

    wrong login attempts added

--- src/services/auth/loginService.js
+++ src/services/auth/loginService.js
@@ -5,8 +5,9 @@ import jwt from "jsonwebtoken";
 export const loginService = async (data) => {
   const { email, password } = data;
   const user = await isUserExist({ key: "email", value: email }, true);
+  if (user?.isBlocked) throw new Error("This user is blocked! Please change your password for unblock your account.");
   if (!user?.isEmailVerified) throw new Error("This user's email is not verified!");
-  await validatePassword(password, user.password);
+  await validatePassword(password, user);
   const token = jwt.sign({ id: user.id, email: user.email, username: user?.username, fullname: user?.fullname }, process.env.JWT_SECRET_KEY, {
     expiresIn: "1h",
   });
--- src/utils/validatePassword.js
+++ src/utils/validatePassword.js
@@ -1,11 +1,31 @@
 import bcrypt from "bcrypt";
+import prisma from "./prisma.js";
 
-export const validatePassword = async (unHashedPassword, hashedPassword) => {
-  const password = await bcrypt.compare(unHashedPassword, hashedPassword);
+export const validatePassword = async (unHashedPassword, user) => {
+  const userUpdate = async (block, number) => {
+    return await prisma.user.update({
+      where: { id: user?.id },
+      data: {
+        isBlocked: block,
+        wrongLoginAttempts: number,
+      },
+    });
+  };
+
+  const password = await bcrypt.compare(unHashedPassword, user?.password);
+  const wrongLoginAttempsNumber = user?.wrongLoginAttempts + 1;
 
   if (!password) {
-    throw new Error("Invalid password");
+    if (wrongLoginAttempsNumber === 5) {
+      await userUpdate(true, wrongLoginAttempsNumber);
+      throw new Error("Your account is blocked! Change your password for unblock your account.");
+    } else {
+      await userUpdate(false, wrongLoginAttempsNumber);
+      throw new Error(`Invalid Password! You have ${5 - wrongLoginAttempsNumber} incorrect entries left.`);
+    }
   }
+
+  await userUpdate(false, 0);
 
   return password;
 };
